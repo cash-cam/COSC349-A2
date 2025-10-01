f02c7eca4c3541c7176b055080dfd6ecf30cbc13
Cam <cam00clark@gmail.com>
2025-10-01 21:24:21 +1300

    changes to db.php add .env.example

--- www/db.php
+++ www/db.php
@@ -1,15 +1,9 @@
 <?php
-$DB_HOST   = '192.168.56.13';
-$DB_NAME   = 'studentdata';
-$DB_USER   = 'webuser';
-$DB_PASSWD = 'testing_password';
-
-$pdo = new PDO(
-  "mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8mb4",
-  $DB_USER,
-  $DB_PASSWD,
-  [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-  ]
-);
+$DB_HOST = getenv('DB_HOST');
+$DB_NAME = getenv('DB_NAME');
+$DB_USER = getenv('DB_USER');
+$DB_PASS = getenv('DB_PASS');
+$pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8mb4",
+               $DB_USER, $DB_PASS,
+               [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC ]);
