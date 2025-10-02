bca059fb4a9bc8af4f1ba17e45453a0e52b78a58
Cam <cam00clark@gmail.com>
2025-10-02 20:17:01 +1300

    minor naming changes around cloning the repo into ec2 api instance

--- infra/api-userdata.sh
+++ infra/api-userdata.sh
@@ -14,8 +14,9 @@ echo 'Acquire::ForceIPv4 "true";' | tee /etc/apt/apt.conf.d/99force-ipv4 >/dev/n
 apt-get update -y
 apt-get install -y apache2 php libapache2-mod-php php-mysql git unzip mysql-client
 
-rm -rf /tmp/a2app
-git clone "$REPO_HTTPS" /tmp/a2app
+#
+rm -rf /tmp/COSC349-A2
+git clone "$REPO_HTTPS" /tmp/COSC349-A2
 
 # Ship API subtree only need to do this as had rsync issues with the .git files
 rsync -a --delete --exclude='.git/' /tmp/COSC349-A2/api/ /var/www/html/
@@ -31,6 +32,8 @@ cat >/etc/apache2/sites-available/api.conf <<EOF
     AllowOverride All
     Require all granted
   </Directory>
+  DirectoryIndex index.php
+
   SetEnv DB_HOST ${RDS_ENDPOINT}
   SetEnv DB_NAME ${DB_NAME}
   SetEnv DB_USER ${DB_USER}
@@ -45,5 +48,6 @@ a2ensite api.conf >/dev/null
 systemctl reload apache2
 
 # 
-[ -f /var/www/html/../db/schema.sql ] && \
-mysql -h "$RDS_ENDPOINT" -u "$DB_USER" "-p${DB_PASS}" "$DB_NAME" < /var/www/html/../db/schema.sql || true
+if [ -f /tmp/COSC349-A2/db/schema.sql ]; then 
+  mysql -h "${RDS_ENDPOINT}" -u "${DB_USER}" "-p${DB_PASS}" "${DB_NAME}" < /tmp/COSC349-A2/db/schema.sql || true
+fi
