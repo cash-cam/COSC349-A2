10f3d1a0088df2201e55ed0d2540fc7ae0940474
Cam <cam00clark@gmail.com>
2025-10-02 19:14:43 +1300

    deleting redunant files from A1

--- app/www/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$DB_HOST = getenv('DB_HOST');
-$DB_NAME = getenv('DB_NAME');
-$DB_USER = getenv('DB_USER');
-$DB_PASS = getenv('DB_PASS');
-$pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8mb4",
-               $DB_USER, $DB_PASS,
-               [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC ]);
